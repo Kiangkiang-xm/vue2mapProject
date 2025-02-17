06a43109301c18d318ddac856d9df23264ec95cb
Kiangkiang-xm <136006569@qq.com>
2025-02-17 22:43:18 +0800

    feat:中国地图

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,129 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="map-container" style="width: 100%; height: 600px;">
+    <div id="map" style="width: 100%; height: 100%;"></div>
   </div>
 </template>
 
 <script>
+import * as echarts from "echarts";
+import chinaMapData from "@/assets/map/china.json"; // 引入 china.json 文件
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      dataList: [
+        { name: "南海诸岛", value: 0 },
+        { name: "北京", value: 100 },
+        { name: "天津", value: 200 },
+        { name: "上海", value: 300 },
+        { name: "重庆", value: 400 },
+        { name: "河北", value: 500 },
+        { name: "河南", value: 600 },
+        { name: "云南", value: 700 },
+        { name: "辽宁", value: 800 },
+        { name: "黑龙江", value: 900 },
+        { name: "湖南", value: 1000 },
+        { name: "安徽", value: 1100 },
+        { name: "山东", value: 1200 },
+        { name: "新疆", value: 1300 },
+        { name: "江苏", value: 1400 },
+        { name: "浙江", value: 1500 },
+        { name: "江西", value: 1600 },
+        { name: "湖北", value: 1700 },
+        { name: "广西", value: 1800 },
+        { name: "甘肃", value: 1900 },
+        { name: "山西", value: 2000 },
+        { name: "内蒙古", value: 2100 },
+        { name: "陕西", value: 2200 },
+        { name: "吉林", value: 2300 },
+        { name: "福建", value: 2400 },
+        { name: "贵州", value: 2500 },
+        { name: "广东", value: 2600 },
+        { name: "青海", value: 2700 },
+        { name: "西藏", value: 2800 },
+        { name: "四川", value: 2900 },
+        { name: "宁夏", value: 3000 },
+        { name: "海南", value: 3100 },
+        { name: "台湾", value: 3200 },
+        { name: "香港", value: 3300 },
+        { name: "澳门", value: 3400 },
+      ],
+    };
+  },
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      // 注册地图
+      echarts.registerMap("china", chinaMapData);
+
+      // 初始化 ECharts 实例
+      const myChart = echarts.init(document.getElementById("map"));
+
+      // 地图配置
+      const option = {
+        tooltip: {
+          formatter: (params) => {
+            return `${params.seriesName}<br/>${params.name}：${params.value}`;
+          },
+        },
+        visualMap: {
+          min: 0,
+          max: 3500,
+          left: "left",
+          top: "bottom",
+          text: ["高", "低"],
+          inRange: {
+            color: ["#e0ffff", "blue"],
+          },
+          show: true,
+        },
+        geo: {
+          map: "china",
+          roam: false,
+          zoom: 1,
+          label: {
+            normal: {
+              show: true,
+              fontSize: "10",
+              color: "rgba(0,0,0,0.7)",
+            },
+          },
+          itemStyle: {
+            normal: {
+              borderColor: "rgba(0, 0, 0, 0.2)",
+            },
+            emphasis: {
+              areaColor: "skyblue",
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              shadowBlur: 20,
+              borderWidth: 0,
+              shadowColor: "rgba(0, 0, 0, 0.5)",
+            },
+          },
+        },
+        series: [
+          {
+            name: "省份",
+            type: "map",
+            geoIndex: 0,
+            data: this.dataList,
+          },
+        ],
+      };
+
+      // 设置地图数据
+      myChart.setOption(option);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.map-container {
+  margin: auto;
 }
 </style>
